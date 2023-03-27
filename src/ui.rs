7a47a828a0fad145b9f0d874c824d79a535f9f2a
Luke Scanlan <luke@lukescanlan.com>
2023-03-27 15:55:16 +0100

    Add sketch of UI logic

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,35 @@
+// use crate::logic::{Cell, SIZE};
+
 use super::logic::Puzzle;
 
 pub fn run() {
     let p = Puzzle::new();
     println!("{}", p.solution().unwrap());
 }
+
+// // pub fn take_input(input: [[Cell; SIZE]; SIZE]) -> Result<Puzzle, _> {
+// //     // get input etc etc
+// //     Puzzle::from(input)?;
+// // }
+
+// fn prompt(opts: &str, default: char) -> char {
+//     default
+// }
+
+// pub fn game_loop() {
+//     // solve or play
+//     let choice = prompt("ps", 'p');
+
+//     if choice == 'p' {
+//         // difficulty
+//         let difficulty = prompt("emh", 'm');
+
+//         // let p = Puzzle::new(difficulty);
+
+//         loop {
+//             // let coord = get_coord();
+//             // let val = get_val();
+//             // p.update(coord, val);
+//         }
+//     }
+// }
