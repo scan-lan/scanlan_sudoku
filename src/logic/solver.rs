ff1e74519c92a275c2756d0d54eec90b42234254
Luke Scanlan <luke@lukescanlan.com>
2023-04-19 17:52:26 +0100

    Create working solver
    Finally

--- src/logic/solver.rs
+++ src/logic/solver.rs
@@ -1,79 +1,70 @@
-use std::time::Duration;
+use std::{cmp::Ordering, time::Duration};
 
-use super::{puzzle::Coord, Cell, Grid};
-
-/// Represents a decision taken by the algorithm
-pub struct Decision {
-    pub cell: Coord,
-    pub val: Cell,
-    pub candidates_changed: Vec<Coord>,
-    pub prev_cell_candidates: Vec<u8>,
-    pub forced: bool,
-}
-
-impl Decision {
-    fn new(
-        cell: Coord,
-        val: Cell,
-        candidates_changed: Vec<Coord>,
-        prev_cell_candidates: Vec<u8>,
-        forced: bool,
-    ) -> Self {
-        Decision {
-            cell,
-            val,
-            candidates_changed,
-            prev_cell_candidates,
-            forced,
-        }
-    }
-}
-
-impl std::fmt::Debug for Decision {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "<({}, {}), {}>", self.cell.row, self.cell.col, self.val)?;
-        Ok(())
-    }
-}
+use super::{puzzle::Coord, Grid};
+const DEBUG_MODE: bool = false;
 
 pub fn solve_backtracking_heuristics(mut g: Grid) -> Option<Grid> {
-    let mut history: Vec<Decision> = Vec::new();
+    let mut history: Vec<(Grid, u8, Coord)> = Vec::new();
     let mut i = 0;
 
     'outer: while !g.solved {
-        std::thread::sleep(Duration::from_millis(500));
-        i += 1;
-        println!("{:=^79}", format!("Iteration {i}"),);
+        if DEBUG_MODE {
+            std::thread::sleep(Duration::from_millis(200));
+            i += 1;
+            println!("{:=^79}", format!("Iteration {i}"),);
+        }
 
         // Get cell with least valid candidates
         let target = g.get_min_candidates_cell();
-        let prev_cell_candidates = g.candidates_at(target);
+        let candidates = g.candidates_at(target);
 
-        // For debugging
-        println!("Grid:\n{g}");
-        println!("Candidate Matrix:\n{}", g.candidate_matrix());
-        println!("Solving {target}");
-        println!("Candidates: {:?}", g.candidates_at(target));
-        println!("History: {:?}", history);
-
-        // Iterate over all candidates
-        for val in g.candidates_at(target).into_iter() {
-            if let Ok(candidates_changed) = g.update(target, val) {
-                // If candidate valid, push decision onto history stack; continue while loop
-                history.push(Decision::new(
-                    target,
-                    Cell::Filled(val),
-                    candidates_changed,
-                    prev_cell_candidates,
-                ));
-                continue 'outer;
-            }
+        if DEBUG_MODE {
+            println!("Grid:\n{g}");
+            println!("Candidate Matrix:\n{}", g.candidate_matrix());
+            println!("Solving {target}");
+            println!("Candidates: {:?}", candidates);
+            // println!("History: {:?}", history);
         }
 
-        // No values were accepted in the for loop, so undo last decision
-        if let Some(last_decision) = history.pop() {
-            g.undo(last_decision);
+        match candidates.len().cmp(&1) {
+            Ordering::Greater => {
+                let mut backtrack = g.clone();
+                for val in candidates.clone().into_iter() {
+                    if let Ok(candidates_changed) = g.update(target, val) {
+                        // If candidate valid, push decision onto history stack; continue while loop
+                        history.push((backtrack, val, target));
+                        continue 'outer;
+                    }
+                }
+            }
+            Ordering::Equal => match g.update(target, candidates[0]) {
+                Ok(candidates_changed) => {
+                    // history.push(Decision::new(
+                    //     target,
+                    //     Cell::Filled(candidates[0]),
+                    //     candidates_changed,
+                    //     candidates,
+                    //     true,
+                    // ));
+                }
+                Err(e) => {
+                    // println!("{e}");
+                    // while history.last().unwrap().forced {
+                    let dec = history.pop().expect("Shouldn't run out of history");
+                    g = dec.0;
+
+                    g.remove_candidate(dec.2, dec.1);
+
+                    // }
+                }
+            },
+            Ordering::Less => panic!("God help us"),
         }
     }
+
+    // No values were accepted in the for loop, so undo last decision
+    // if let Some(last_decision) = history.pop() {
+    //     g.undo(last_decision);
+    // }
     Some(g)
 }
