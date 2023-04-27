a8463d8d8bf8f050adc6311fdef28431fdc22358
Luke Scanlan <luke@lukescanlan.com>
2023-04-27 13:02:28 +0100

    Improve documentation/readability of solver

--- src/logic/solver.rs
+++ src/logic/solver.rs
@@ -2,9 +2,16 @@ use std::cmp::Ordering;
 
 use super::{Coord, Grid};
 
+/// Struct to model a decision point in the algorithm.
+struct Decision {
+    state: Grid,
+    val: u8,
+    target: Coord,
+}
+
 /// Solve grid `g` using a backtracking algorithm with heurisitics
 pub fn solve_backtracking_heuristics(mut g: Grid) -> Option<Grid> {
-    let mut history: Vec<(Grid, u8, Coord)> = Vec::new();
+    let mut history: Vec<Decision> = Vec::new();
 
     'outer: while !g.solved {
         // Get cell with least valid candidates
@@ -12,24 +19,37 @@ pub fn solve_backtracking_heuristics(mut g: Grid) -> Option<Grid> {
         let candidates = g.candidates_at(target);
 
         match candidates.len().cmp(&1) {
+            // If the cell has more than one candidate, the choice we make
+            // may be wrong, so we store the current state of the grid.
             Ordering::Greater => {
                 let backtrack = g.clone();
 
                 for val in candidates.clone().into_iter() {
                     if g.update(target, val).is_ok() {
                         // If candidate valid, push decision onto history stack; continue while loop
-                        history.push((backtrack, val, target));
+                        history.push(Decision {
+                            state: backtrack,
+                            val,
+                            target,
+                        });
+
                         continue 'outer;
                     }
                 }
             }
+            // If the cell has exactly one candidate, that's not recorded as
+            // a decision point, because we're choosing the only option.
+            // However, if updating the grid with that option fails,
+            // we know a previous decision was incorrect, so we backtrack.
             Ordering::Equal => {
                 if g.update(target, candidates[0]).is_err() {
-                    // println!("{e}");
                     let dec = history.pop().expect("Shouldn't run out of history");
-                    g = dec.0;
 
-                    g.remove_candidate(dec.2, dec.1);
+                    // Set grid to the state at the last decision point, then
+                    // remove the value that was chosen for the target as a
+                    // candidate, because it led to an error.
+                    g = dec.state;
+                    g.remove_candidate(dec.target, dec.val);
                 }
             }
             Ordering::Less => panic!("This should never happen"),
